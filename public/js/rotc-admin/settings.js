9412158a7441c22a1d8ba1bfffdd7b6fe0c2cbcb
Realyn <realynvistal1@gmail.com>
2026-08-08 18:57:54 +0800

    Help me God

--- public/js/rotc-admin/settings.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>bootstrapPortalPage({expectedPortal:"rotc-admin",shellRole:"rotc",moduleSrc:"/assets/js/common/settings-page.js",render:async(content)=>renderSettingsPage(content,"account")}));
