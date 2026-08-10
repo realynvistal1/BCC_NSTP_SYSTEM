26859ce289b53402bd529044a444bcf0ce96e13b
Realyn <realynvistal1@gmail.com>
2026-08-10 18:25:04 +0800

    Update CWTS admin routes

--- routes/cwtsAdminRoutes.js
+++ routes/cwtsAdminRoutes.js
@@ -12,6 +12,7 @@ router.get("/enrollments", controller.enrollments);
 router.get("/enrollments/:id", controller.enrollmentDetail);
 router.patch("/enrollments/:id", controller.updateEnrollment);
 router.post("/enrollments/bulk-approve", controller.bulkApprove);
+router.post("/enrollments/bulk-reject", controller.bulkReject);
 router.get("/roster", controller.roster);
 router.post("/auto-assign", controller.autoAssign);
 router.get("/grades", controller.grades);
