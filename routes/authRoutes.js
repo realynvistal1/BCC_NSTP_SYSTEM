d0df8c0e835a05e268919752429ef631bee5116e
Realyn <realynvistal1@.com>
2026-08-11 17:41:03 +0800

    Update auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,10 @@
-const r=require('express').Router();
-const c=require('../controllers/authController');
-const {
-  requireAuth
-}
-=require('../middleware/authMiddleware');
-r.post('/login',c.login);
-r.post('/logout',c.logout);
-r.get('/me',requireAuth,c.me);
-r.post('/change-password',requireAuth,c.changePassword);
-module.exports=r;
+const router = require('express').Router();
+const controller = require('../controllers/authController');
+const { requireAuth } = require('../middleware/authMiddleware');
+
+router.post('/login', controller.login);
+router.post('/logout', controller.logout);
+router.get('/me', requireAuth, controller.me);
+router.post('/change-password', requireAuth, controller.changePassword);
+
+module.exports = router;
